1f83e3b8546727b7078b439b7fd6bf82e22e351a
Jakub Jendryka <jendryka.j@gmail.com>
2018-04-12 19:30:03 +0200

    Implemented rendering of chunks

--- src/world/chunk/Chunk.cpp
+++ src/world/chunk/Chunk.cpp
@@ -1,10 +1,14 @@
 #include "Chunk.h"
+#include "../../tile/TileDatabase.h"
 
 #include <fstream>
 #include <iostream>
 
-void Chunk::render(sf::RenderWindow) {
+void Chunk::render(sf::RenderWindow& window) {
+    sf::RenderStates states;
+    states.texture = &TileDatabase::get().texture;
 
+    window.draw(vertices, states);
 }
 
 void Chunk::update(std::chrono::microseconds deltaTime) {
@@ -23,6 +27,7 @@ int Chunk::getTile(int x, int y) {
 
 void Chunk::setTile(int x, int y, int value) {
     tiles[y*SIDE_LENGTH + x] = value;
+    changeQuad(x, y);
 }
 
 void Chunk::save(const std::string &fileName) {
@@ -55,7 +60,48 @@ bool Chunk::load(const std::string &filename) {
         }
     }
     tileData.close();
+
+    generateVertices();
+
     return true;
+}
+
+void Chunk::generateVertices() {
+
+    vertices.setPrimitiveType(sf::Quads);
+    vertices.resize(SIDE_LENGTH*SIDE_LENGTH*4);
+
+    for (unsigned int i=0; i<SIDE_LENGTH; i++) {
+        for (unsigned int j=0; j<SIDE_LENGTH; j++) {
+            sf::Vertex* quad = &vertices[(i+j*SIDE_LENGTH) * 4];
+
+            quad[0].position = sf::Vector2f(i*TILE_SIZE, j*TILE_SIZE);
+            quad[1].position = sf::Vector2f((i+1)*TILE_SIZE, j*TILE_SIZE);
+            quad[2].position = sf::Vector2f((i+1)*TILE_SIZE, (j+1)*TILE_SIZE);
+            quad[3].position = sf::Vector2f(i*TILE_SIZE, (j+1)*TILE_SIZE);
+
+            changeQuad(i, j);
+        }
+    }
+}
+
+void Chunk::changeQuad(int x, int y) {
+    if (vertices.getVertexCount() == 0) {
+        generateVertices();
+    }
+    else {
+        sf::Vertex *quad = &vertices[(x + y * SIDE_LENGTH) * 4];
+
+        int tile_id = getTile(x, y);
+        std::cout<<tile_id<<std::endl;
+        int tx = TileDatabase::get()[tile_id].texture_x;
+        int ty = TileDatabase::get()[tile_id].texture_y;
+
+        quad[0].texCoords = sf::Vector2f(tx*TILE_SIZE, ty*TILE_SIZE);
+        quad[1].texCoords = sf::Vector2f((tx+1)*TILE_SIZE, ty*TILE_SIZE);
+        quad[2].texCoords = sf::Vector2f((tx+1)*TILE_SIZE, (ty+1)*TILE_SIZE);
+        quad[3].texCoords = sf::Vector2f(tx*TILE_SIZE, (ty+1)*TILE_SIZE);
+    }
 }
 
 
--- src/world/chunk/Chunk.h
+++ src/world/chunk/Chunk.h
@@ -3,6 +3,7 @@
 
 
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
 #include <vector>
 #include <chrono>
 #include "../object/Object.h"
@@ -10,14 +11,19 @@
 class Chunk {
 public:
     static const int SIDE_LENGTH = 32;
+    static const int TILE_SIZE = 16;
 private:
     std::array<int, SIDE_LENGTH*SIDE_LENGTH> tiles;
     std::vector<std::unique_ptr<Object>> objects;
+
+    void generateVertices();
+    void changeQuad(int x, int y);
+    sf::VertexArray vertices;
 public:
     Chunk(Chunk const&) = delete;
     void operator= (Chunk const&) = delete;
 
-    void render(sf::RenderWindow);
+    void render(sf::RenderWindow& window);
     void update(std::chrono::microseconds deltaTime);
 
     void save(const std::string& filename);
