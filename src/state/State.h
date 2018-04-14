9996560ad2b4097901deac3be068c0ff4061f5c0
Jakub Jendryka <jendryka.j@gmail.com>
2018-04-14 15:33:18 +0200

    Documented states

--- src/state/State.h
+++ src/state/State.h
@@ -4,12 +4,31 @@
 #include <chrono>
 #include <SFML/Graphics/RenderWindow.hpp>
 
+//!Base class for program states
+/*!
+ * Represents a program state like menu or game. Kept on stack by Game.
+ */
 class State {
 protected:
     State() = default;
 public:
+    //!Function called every loop iteration
+    /*!
+     * update() is called every main loop iteration. Should be used for implementing stuff like physics etc.
+     * @param deltaTime Time which passed between update() calls
+     */
     virtual void update(std::chrono::microseconds deltaTime) = 0;
+    //! Function called every screen refresh
+    /*!
+     * render() is called every screen refresh. Should be used for rendering.
+     * @param renderWindow SFML render window reference
+     */
     virtual void render(sf::RenderWindow* renderWindow) = 0;
+    //! Function called periodicaly
+    /*!
+     *  tick() is called periodicaly. Period is set in Game singleton.
+     *  Can be used for stuff that doesn't need to run very often, for example recalculating AI path.
+     */
     virtual void tick() = 0;
 
     State(State& other) = delete;
