af066e47466b05933165e1da57112386140206f5
Jakub Jendryka <jendryka.j@gmail.com>
2018-04-15 15:33:15 +0200

    Added global tile setter and getter for world

--- src/world/World.cpp
+++ src/world/World.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "World.h"
+#include "../tile/Tile.h"
+#include "../utils/Log.h"
 
 World::World(int seed) : chunkDatabase(std::make_unique<ChunkGenerator>(seed)){
 
@@ -20,4 +22,41 @@ void World::render(sf::RenderWindow &window, sf::View camera) {
         }
     }
     ChunkGenerator chunkGenerator(10);
+}
+
+int World::getTile(int x, int y) {
+    sf::Vector2i tile;
+    tile.x = abs(x);
+    tile.y = abs(y);
+    sf::Vector2i chunk = tile / Chunk::SIDE_LENGTH;
+    tile.x = tile.x % Chunk::SIDE_LENGTH;
+    tile.y = tile.y % Chunk::SIDE_LENGTH;
+    if (x<0) {
+        chunk.x = -chunk.x - 1;
+        tile.x = Chunk::SIDE_LENGTH - tile.x;
+    }
+    if (y<0) {
+        chunk.y = -chunk.y - 1;
+        tile.y = Chunk::SIDE_LENGTH - tile.y;
+    }
+    return chunkDatabase.getChunk(chunk.x, chunk.y)->getTile(tile.x, tile.y);
+}
+
+void World::setTile(int x, int y, int id) {
+    Log::verbose(TAG, "Tile set at: " + std::to_string(x) + " " + std::to_string(y) + " to: " + std::to_string(id));
+    sf::Vector2i tile;
+    tile.x = abs(x);
+    tile.y = abs(y);
+    sf::Vector2i chunk = tile / Chunk::SIDE_LENGTH;
+    tile.x = tile.x % Chunk::SIDE_LENGTH;
+    tile.y = tile.y % Chunk::SIDE_LENGTH;
+    if (x<0) {
+        chunk.x = -chunk.x - 1;
+        tile.x = Chunk::SIDE_LENGTH - tile.x;
+    }
+    if (y<0) {
+        chunk.y = -chunk.y - 1;
+        tile.y = Chunk::SIDE_LENGTH - tile.y;
+    }
+    chunkDatabase.getChunk(chunk.x, chunk.y)->setTile(tile.x, tile.y, id);
 }
