ee1b0d03f7f4ba6953995f92d0db5c331565d872
Jakub Jendryka <jendryka.j@gmail.com>
2018-05-05 18:56:46 +0200

    Fixed player movement

--- src/entity/Player.cpp
+++ src/entity/Player.cpp
@@ -6,7 +6,7 @@
 #include "../tile/TileDatabase.h"
 
 Player::Player(GameState& game_state)
-        : Entity(0, std::string("Player"), sf::Vector2f(0,100), sf::FloatRect(0, 32, 16, 32), sf::Texture(), game_state) {
+        : Entity(0, std::string("Player"), sf::Vector2f(0,0), sf::FloatRect(0, 32, 16, 32), sf::Texture(), game_state) {
 
     texture.loadFromFile("res/textures/player.png");
     sprite = sf::Sprite(texture);
@@ -17,36 +17,63 @@ Player::Player(GameState& game_state)
 
 void Player::update(std::chrono::microseconds deltaTime) {
 
-    auto div = [](int a, int b)->int{int ret = a/b; return ret>=0? ret: ret-1;};
-
     const float MOVEMENT_DIV = 2000.0f;
 
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W) && touching_ground){
+    if((sf::Keyboard::isKeyPressed(sf::Keyboard::Space) || sf::Keyboard::isKeyPressed(sf::Keyboard::W)) && touching_ground){
         speed.y = 1.0f;
         touching_ground = false;
     }
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
-        position += sf::Vector2f(-deltaTime.count()/MOVEMENT_DIV, 0.0f);
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A) && speed.x > -0.6f){
+        speed.x -= 0.005f * deltaTime.count() / MOVEMENT_DIV;
     }
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
-        position += sf::Vector2f(deltaTime.count()/MOVEMENT_DIV, 0.0f);
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D) && speed.x < 0.6f){
+        speed.x += 0.005f * deltaTime.count() / MOVEMENT_DIV;
     }
-
-    if (!touching_ground) {
-        speed.y -= 0.005f * deltaTime.count() / MOVEMENT_DIV;
-    }
-
-    position.y += deltaTime.count() * speed.y / MOVEMENT_DIV;
-
-    if (TileDatabase::get()[game_state.getWorld().getTile((int)position.x / Chunk::TILE_SIZE, div((int)position.y, Chunk::TILE_SIZE)).tileId].isSolid) {
-        touching_ground = true;
-        speed.y = 0;
-        position.y = (int)(position.y / Chunk::TILE_SIZE) * Chunk::TILE_SIZE;
-        if (TileDatabase::get()[game_state.getWorld().getTile((int)position.x / Chunk::TILE_SIZE, (int)position.y / Chunk::TILE_SIZE).tileId].isSolid) {
-            position.y = ((int)(position.y / Chunk::TILE_SIZE) + 1 )* Chunk::TILE_SIZE;
+    if(!sf::Keyboard::isKeyPressed(sf::Keyboard::A) && !sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+        if (fabs(speed.x) > 0.005f * deltaTime.count() / MOVEMENT_DIV) {
+            speed.x += speed.x > 0 ? (-0.005f * deltaTime.count() / MOVEMENT_DIV) : (0.005f * deltaTime.count() / MOVEMENT_DIV);
+        } else {
+            speed.x = 0;
         }
     }
-    else {
+
+
+    auto div = [](float a, int b)->int{int ret = a/b; return a>=0 ? ret : ret-1;};
+
+
+    float old_pos_x = position.x;
+    position.x += speed.x * deltaTime.count() / MOVEMENT_DIV;
+
+    Tile tile_d = TileDatabase::get()[game_state.getWorld().getTile(                           //Collision detection X
+            div(position.x + sprite.getGlobalBounds().width/2, Chunk::TILE_SIZE),
+            div(position.y, Chunk::TILE_SIZE)).tileId];
+    Tile tile_u = TileDatabase::get()[game_state.getWorld().getTile(
+            div(position.x + sprite.getGlobalBounds().width/2, Chunk::TILE_SIZE),
+            div(position.y, Chunk::TILE_SIZE) + 1).tileId];
+
+    if (tile_d.isSolid && !tile_u.isSolid) {
+        position.y = (div(position.y, Chunk::TILE_SIZE) + 1) * Chunk::TILE_SIZE + 0.0001f;
+    }
+    if (tile_d.isSolid && tile_u.isSolid) {
+        position.x = old_pos_x;
+        speed.x = 0;
+    }
+
+    speed.y -= 0.005f * deltaTime.count() / MOVEMENT_DIV; //Gravity
+
+    float old_pos_y = position.y;
+    position.y += speed.y * deltaTime.count() / MOVEMENT_DIV;
+
+    Tile tile = TileDatabase::get()[game_state.getWorld().getTile(                           //Collision detection Y
+            div(position.x + sprite.getGlobalBounds().width/2, Chunk::TILE_SIZE),
+            div(position.y, Chunk::TILE_SIZE)).tileId];
+
+    if (tile.isSolid) {
+        position.y = old_pos_y;
+        touching_ground = true;
+        speed.y = 0;
+    } else {
         touching_ground = false;
     }
+
 }
