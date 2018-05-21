e2deb8fec82ed6d5b8d7d9b84becfb5a58731615
zakrent <szymon.zakrent@gmail.com>
2018-05-21 12:27:44 +0200

    Added debug class

---
+++ src/utils/Debug.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zakrent on 5/21/18.
+//
+
+#include "Debug.h"
---
+++ src/utils/Debug.h
@@ -0,0 +1,16 @@
+//
+// Created by zakrent on 5/21/18.
+//
+
+#ifndef NONAME_DEBUG_H
+#define NONAME_DEBUG_H
+
+
+
+class Debug {
+
+};
+
+
+
+#endif //NONAME_DEBUG_H
